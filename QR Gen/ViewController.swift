2d29adf48aee05611e4bb0e76cd153d3b2176342
Andrii Halabuda <andrii.halabuda@gmail.com>
2020-03-11 21:30:58 +0100

    Initial Commit

---
+++ QR Gen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QR Gen
+//
+//  Created by Andrew on 3/11/20.
+//  Copyright © 2020 Andrii Halabuda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
